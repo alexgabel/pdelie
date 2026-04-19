5a97cdaae505e8d16c1c3fb41f1b8134cea01e8c
Alex Gabel <alex_gabel@hotmail.com>
2026-04-20 00:24:09 +0200

    Added runtime symbolic rendering helpers for GeneratorFamily

--- src/pdelie/symmetry/__init__.py
+++ src/pdelie/symmetry/__init__.py
@@ -1,4 +1,8 @@
 from pdelie.symmetry.fitting.translation_baseline import fit_translation_generator
+from pdelie.symmetry.symbolic import render_generator_family, to_sympy_component_expressions
 
-__all__ = ["fit_translation_generator"]
-
+__all__ = [
+    "fit_translation_generator",
+    "render_generator_family",
+    "to_sympy_component_expressions",
+]
